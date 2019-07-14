51f263e16942c1489a6885e299aaab139bf39a0a
Andres Choque <andreschoquemx0@gmail.com>
2019-07-13 22:12:40 -0500

    Crear swiper de tarjetas
    new Swiper()

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Pelicular',
+      title: 'Pelicula',
       initialRoute: '/',
       routes:{
         '/':  (BuildContext context)=>HomePage()
