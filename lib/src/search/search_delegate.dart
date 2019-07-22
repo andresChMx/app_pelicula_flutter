d79faf3d5c01b5eadacbd1b6113c879857444233
Andres Choque <andreschoquemx0@gmail.com>
2019-07-22 11:48:59 -0500

    BuildActions y BuildLeading

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
-class DataSearch extends SearchDelegate{
+
+class DataSearch extends SearchDelegate {
   @override
   List<Widget> buildActions(BuildContext context) {
     // Las acciones de nuestro appbar(icono para limpiar el campo)
-    return null;
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query='';
+        },
+      )
+    ];
   }
 
   @override
   Widget buildLeading(BuildContext context) {
     // icono a la izquierda del appbar
-    return null;
+    return IconButton(
+      icon:Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context,null);
+      },
+    );
   }
 
   @override
   Widget buildResults(BuildContext context) {
     // Crea los resultados que vamos a mostrar
-    return null;
+    return Container();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Son las sugerencias que aparecen cuando la persona escribe
-    return null;
+    return Container();
   }
-
 }
