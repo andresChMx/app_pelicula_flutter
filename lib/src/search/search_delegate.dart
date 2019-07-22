fb1b3f38139c163d9ab909ef4802ff4b47f16ed1
Andres Choque <andreschoquemx0@gmail.com>
2019-07-22 10:50:31 -0500

    Search Delegate

---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+class DataSearch extends SearchDelegate{
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // Las acciones de nuestro appbar(icono para limpiar el campo)
+    return null;
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // icono a la izquierda del appbar
+    return null;
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Crea los resultados que vamos a mostrar
+    return null;
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Son las sugerencias que aparecen cuando la persona escribe
+    return null;
+  }
+
+}
