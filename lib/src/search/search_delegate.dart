60b90b6a5d8b3c191babef92179957cf09ea0aee
Andres Choque <andreschoquemx0@gmail.com>
2019-07-24 18:00:40 -0500

    Buscar peticion en ThemovieDB

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,7 +1,10 @@
+import 'package:app_pelicula_seccion7/src/models/pelicula_model.dart';
+import 'package:app_pelicula_seccion7/src/providers/peliculas_provider.dart';
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate {
   String seleccion='';
+  final peliculasProvider=new PeliculasProvider();
   final peliculas = [
     'Avengers',
     'Xmen',
@@ -51,30 +54,64 @@ class DataSearch extends SearchDelegate {
       ),
     );
   }
-
   @override
-  Widget buildSuggestions(BuildContext context) {
-    // Son las sugerencias que aparecen cuando la persona escribe
-    final listaSugerida = (query.isEmpty)
-        ? peliculasRecienes
-        : peliculas.where((p) {
-            return p.toLowerCase().startsWith(query.toLowerCase());
-          }).toList();
-
-    return ListView.builder(
-      itemCount: listaSugerida.length,
-      itemBuilder: (context, i) {
-        return ListTile(
-          leading: Icon(Icons.movie),
-          title: Text(listaSugerida[i]),
-          onTap: (){
-            seleccion=listaSugerida[i];
-            showResults(context);
-            
-          },
-        );
+  Widget buildSuggestions(BuildContext context){
+    if(query.isEmpty){
+      print("esta vacio");
+      return Container();
+    }
+    return FutureBuilder(
+      future:peliculasProvider.buscarPelicula(query),
+      builder: (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot){
+        if(snapshot.hasData){
+          return ListView(
+            children: snapshot.data.map((pelicula){
+              return ListTile(
+                leading: FadeInImage(
+                  image: NetworkImage(pelicula.getPostImg()),
+                  placeholder: AssetImage('assets/img/no-image.jpg'),
+                  width: 50.0,
+                  fit: BoxFit.cover,
+                ),
+                title: Text(pelicula.title),
+                subtitle: Text(pelicula.originalTitle),
+                onTap: (){
+                  close(context, '');
+                  pelicula.uniqueId='';
+                  Navigator.pushNamed(context, 'detalle',arguments:pelicula);
+                },
+              );
+            }).toList()
+          );
+        }else{
+          return Center(child:CircularProgressIndicator());
+        }
       },
-      
     );
   }
+//  @override
+  // Widget buildSuggestions(BuildContext context) {
+  //   // Son las sugerencias que aparecen cuando la persona escribe
+  //   final listaSugerida = (query.isEmpty)
+  //       ? peliculasRecienes
+  //       : peliculas.where((p) {
+  //           return p.toLowerCase().startsWith(query.toLowerCase());
+  //         }).toList();
+
+  //   return ListView.builder(
+  //     itemCount: listaSugerida.length,
+  //     itemBuilder: (context, i) {
+  //       return ListTile(
+  //         leading: Icon(Icons.movie),
+  //         title: Text(listaSugerida[i]),
+  //         onTap: (){
+  //           seleccion=listaSugerida[i];
+  //           showResults(context);
+            
+  //         },
+  //       );
+  //     },
+      
+  //   );
+  // }
 }
