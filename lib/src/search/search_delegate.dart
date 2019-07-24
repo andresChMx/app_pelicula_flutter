dc3e37edc6cbb669e3ca914e2d4a391d0b00a77b
Andres Choque <andreschoquemx0@gmail.com>
2019-07-24 10:45:16 -0500

    BuildSuggestions y BuildResults

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate {
+  String seleccion='';
+  final peliculas = [
+    'Avengers',
+    'Xmen',
+    'Mulan',
+    'Malefica',
+    'Transformers',
+    'Rapidos y furiosos'
+  ];
+  final peliculasRecienes = [
+    'Spiderman',
+    'Capitan ameirca'
+        'algo mas'
+  ];
   @override
   List<Widget> buildActions(BuildContext context) {
     // Las acciones de nuestro appbar(icono para limpiar el campo)
@@ -8,7 +22,7 @@ class DataSearch extends SearchDelegate {
       IconButton(
         icon: Icon(Icons.clear),
         onPressed: () {
-          query='';
+          query = '';
         },
       )
     ];
@@ -18,9 +32,9 @@ class DataSearch extends SearchDelegate {
   Widget buildLeading(BuildContext context) {
     // icono a la izquierda del appbar
     return IconButton(
-      icon:Icon(Icons.arrow_back),
+      icon: Icon(Icons.arrow_back),
       onPressed: () {
-        close(context,null);
+        close(context, null);
       },
     );
   }
@@ -28,12 +42,39 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // Crea los resultados que vamos a mostrar
-    return Container();
+    return Center(
+      child: Container(
+        height:100.0,
+        width:100.0,
+        color:Colors.blue,
+        child: Text(seleccion),
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Son las sugerencias que aparecen cuando la persona escribe
-    return Container();
+    final listaSugerida = (query.isEmpty)
+        ? peliculasRecienes
+        : peliculas.where((p) {
+            return p.toLowerCase().startsWith(query.toLowerCase());
+          }).toList();
+
+    return ListView.builder(
+      itemCount: listaSugerida.length,
+      itemBuilder: (context, i) {
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(listaSugerida[i]),
+          onTap: (){
+            seleccion=listaSugerida[i];
+            showResults(context);
+            
+          },
+        );
+      },
+      
+    );
   }
 }
